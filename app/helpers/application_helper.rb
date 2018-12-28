399be9551fcec9466168010e381a27fcf5a99ee9
JElbourne <jayelbourne@gmail.com>
2018-12-28 02:10:45 -0500

    Added Logo and SVG helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,16 @@ module ApplicationHelper
       notice: "alert-info"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
+
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    file = assets.find_asset(filename).source.force_encoding("UTF-8")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css "svg"
+    if options[:class].present?
+      svg["class"] = options[:class]
+    end
+    raw doc
+  end
+
 end
